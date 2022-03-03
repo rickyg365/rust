24389702a0de57a1981b8d357223843a9a81fbcd
rickyg365 <rickygonzal3z@hotmail.com>
2022-03-02 22:21:25 -0800

    got through hello triangle

--- opengl/playground/src/main.rs
+++ opengl/playground/src/main.rs
@@ -1,22 +1,34 @@
 extern crate sdl2;
 extern crate gl;
 
+// import namespace to avoid repeating `std::ffi` everywhere
+use std::ffi::{CString, CStr};
+
+pub mod render_gl;
+
 // Raw dog it
 // gl::types::GLfloat
-// alias it
-// type gl_float = gl::types::GLfloat;
 // or 
 // include all the OpenGL type aliases
 // use gl::types::*;
 
+// The _(underscore)  silences the warning about unused value
 
 fn main() {
     // Initialize sdl
-    // The _(underscore)  silences the warning about unused value
     let sdl = sdl2::init().unwrap();
 
     // Initialize video subsystem
     let video_subsystem = sdl.video().unwrap();
+
+    
+    // Specify minimal OpenGL version, using sdl gl attributes
+    let gl_attr = video_subsystem.gl_attr();
+
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4,5);
+
+    // Create Window
     let window = video_subsystem
             .window("Playground", 900, 700)
             .opengl() // add opengl flag
@@ -29,8 +41,74 @@ fn main() {
     let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
     unsafe {
-        gl::ClearColor(0.3, 0.3, 0.5, 0.8);
+        gl::Viewport(0, 0, 900, 700); // set viewport
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
+
+
+    // Shaders
+    let vert_shader = render_gl::Shader::from_vert_source(
+        &CString::new(include_str!("triangle.vert")).unwrap()
+    ).unwrap();
+
+    let frag_shader =
+        render_gl::Shader::from_frag_source(
+            &CString::new(include_str!("triangle.frag")).unwrap()
+        ).unwrap();
+
+    let shader_program = render_gl::Program::from_shaders(
+        &[vert_shader, frag_shader]
+    ).unwrap();
+    
+    // Set up vertex buffer object
+    let vertices: Vec<f32> = vec![
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0,
+        0.0, 0.5, 0.0
+    ];
+
+    let mut vbo: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut vbo);
+    }
+    unsafe {
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER, // target
+            (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr, // size of data in bytes
+            vertices.as_ptr() as *const gl::types::GLvoid, // pointer to data
+            gl::STATIC_DRAW, // usage
+        );
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0); // unbind the buffer
+    }
+
+    // Set up vertex array object
+
+    let mut vao: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+    }
+
+    unsafe {
+        gl::BindVertexArray(vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+
+        gl::EnableVertexAttribArray(0); // this is "layout (location = 0)" in vertex shader
+        gl::VertexAttribPointer(
+            0, // index of the generic vertex attribute ("layout (location = 0)")
+            3, // the number of components per generic vertex attribute
+            gl::FLOAT, // data type
+            gl::FALSE, // normalized (int-to-float conversion)
+            (3 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+            std::ptr::null() // offset of the first component
+        );
+        
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+        
+    }
+
+    // Set up shared state
 
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
@@ -44,6 +122,17 @@ fn main() {
         
         unsafe {
             gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        
+        // Draw Triangle
+        shader_program.set_used();
+        unsafe {
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(
+                gl::TRIANGLES, // mode
+                0, // starting index in the enabled arrays
+                3 // number of indices to be rendered
+            );
         }
 
         // render window contents
