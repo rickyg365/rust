24a33ee86b6c39485c8c7d66abf6e50e509b1ae8
rickyg365 <rickygonzal3z@hotmail.com>
2022-10-15 17:19:23 -0700

    updated style and functionality [style] - 7 -> 5 row digits
    [funtion]
    - no longer infinite loop, prompts user for amount of seconds to run clock

--- clock/src/main.rs
+++ clock/src/main.rs
@@ -1,19 +1,34 @@
 use chrono::{Local};
 
+use std::io::Write;
+
+fn prompt(name:&str) -> String {
+    let mut line = String::new();
+    print!("{}", name);
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut line).expect("Error: Could not read a line");
+
+    return line.trim().to_string()
+}
+
+
+
 // underscore supresses error
-const _DIGITS : [[&str; 11]; 7] = [
-    ["┌───┐", "  ╷  ", "┌───┐", "┌───┐", "╷   ╷", "┌───┐", "┌─── ", "┌───┐", "┌───┐", "┌───┐", "     ", ],
-    ["│   │", "  │  ", "    │", "    │", "│   │", "│    ", "│    ", "    │", "│   │", "│   │", "     ", ],
+const _DIGITS : [[&str; 11]; 5] = [
+    ["╭───╮", "  ╷  ", "╭───╮", "╭───╮", "╷   ╷", "┌───┐", "╭─── ", "┌───┐", "╭───╮", "╭───╮", "     ", ],
     ["│   │", "  │  ", "    │", "    │", "│   │", "│    ", "│    ", "    │", "│   │", "│   │", "  .  ", ],
-    ["│   │", "  │  ", "┌───┘", " ───┤", "└───┤", "└───┐", "├───┐", "    │", "├───┤", "└───┤", "     ", ],
+    ["│   │", "  │  ", "╭───╯", " ───┤", "└───┤", "└───╮", "├───╮", "    │", "├───┤", "╰───┤", "     ", ],
     ["│   │", "  │  ", "│    ", "    │", "    │", "    │", "│   │", "    │", "│   │", "    │", "  .  ", ],
-    ["│   │", "  │  ", "│    ", "    │", "    │", "    │", "│   │", "    │", "│   │", "    │", "     ", ],
-    ["└───┘", "  ╵  ", "└────", "└───┘", "    ╵", "└───┘", "└───┘", "    ╵", "└───┘", "└───┘", "     ", ],
+    ["╰───╯", "  ╵  ", "└────", "╰───╯", "    ╵", "└───╯", "╰───╯", "    ╵", "╰───╯", "╰───╯", "     ", ],
 ];
 
 
 fn main() {
     print!("\x1b[?25l"); // Hide Cursor
+
+    let mut iter = 0;
+    let max_iter = prompt("How many seconds to run clock: ").parse::<i32>().unwrap_or(60);
+
     loop {
         // print!("\x1b[3J"); // Erase Display use 2 or 3
         let t = Local::now();
@@ -34,6 +49,12 @@ fn main() {
         }
         // Pause for 990ms
         std::thread::sleep(std::time::Duration::from_millis(990));
-        print!("\x1b[7A"); // Move Cursor to the top
+        print!("\x1b[5A"); // Move Cursor to the top
+        if iter >= max_iter {
+            break;
+        }
+        iter += 1;
     }
+    print!("\x1b[5B"); // Move Cursor to the bottom
+    println!(); // blank line
 }
